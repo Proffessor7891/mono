260675586718b9b74dd5f82e31fe02a05ea0b647
Dev <d85842285@gmail.com>
2025-07-23 13:05:02 +0300

    env-db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Book } from './book.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT, 10) : 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      database: process.env.POSTGRES_DB || 'books',
+      entities: [Book],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Book]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
